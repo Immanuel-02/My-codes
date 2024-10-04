bc65d2eace09a762d04b62d5e5d11017b74d5547
Immanuel-02 <immanuelphiri234@gmail.com>
2024-10-05 00:01:22 +0800

    Add files via upload

---
+++ Finding Area of circle using functions.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+float area ( float r) 
+float A = 3.142*r*r;
+return A;
+}
+int main( ) {
+    float result = area ( 2) ;
+    cout <<"Result: " << area (2) << endl;
+    
+    
+    return 0;
+}
